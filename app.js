fcb5d64497819f8911c6c45783b1e696c9588e6f
edsan <jackwest415@gmail.com>
2020-09-09 08:41:35 +0300

    adding params into url

--- app.js
+++ app.js
@@ -24,23 +24,52 @@ app.use(cors())
 connectDB()
 
 app.get("/articles", (req, res) => {
+  //query searching the database to return all the document
+
   Article.find({}, (err, foundResults) => {
-    if (!err) {
-      res.send(foundResults)
-    } else {
-      res.send(err)
-    }
+    //the response of the found documents
+
+    !err ? res.send(foundResults) : res.send(err)
   })
 })
 
 app.post("/articles", (req, res) => {
+  //create the new article
+
   const article = new Article({
     title: req.body.title,
     content: req.body.content,
   })
 
+  //save the new article
+
   article.save((err) =>
     !err ? res.send("the document was succesfully send") : res.send(err)
+  )
+})
+
+app.delete("/articles", (req, res) => {
+  Article.deleteMany({}, (err) => {
+    !err ? res.send("the document was succesfully send") : res.send(err)
+  })
+})
+
+app.get("/articles/:articleTitle", (req, res) => {
+  Article.findOne({title: req.params.articleTitle}, (err, foundDoc) => {
+    foundDoc ? res.send(foundDoc) : res.send(err)
+  })
+})
+
+app.put("/articles/:articleTitle", (req, res) => {
+  //this will cause total overwrite of the document
+
+  Article.update(
+    {title: req.params.articleTitle},
+    {title: req.body.title, content: req.body.content},
+    {overwrite: true},
+    (err, changedDoc) => {
+      changedDoc ? res.send(changedDoc) : res.send(err)
+    }
   )
 })
 
